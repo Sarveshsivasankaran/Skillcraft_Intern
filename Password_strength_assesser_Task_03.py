7b3f9076e9bd9a9b1c41d8e99c80d5289d9eabff
Sarveshsivasankaran <sarveshsivasankaran@yahoo.com>
2025-07-22 00:26:02 +0530

    ch

--- Password_strength_assesser_Task_03.py
+++ Password_strength_assesser_Task_03.py
@@ -1,2 +1,8 @@
 import tkinter as tk
 
+root=tk.Tk()
+root.title("Password_strength_assesser")
+root.geometry("400x200")
+tk.Label(text="Enter your Password:",font=('calibre',12,'bold')).pack(pady=10)
+string_input=tk.Entry(root,width=40).pack(pady=20)
+root.mainloop()
